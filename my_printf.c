49e19a1ed93202d354b940672ec3911cea728a9c
quinton mincy <mincy_q@qwasar.io>
2022-08-01 20:49:00 +0000

    my_printf without %c

--- my_printf.c
+++ my_printf.c
@@ -7,7 +7,7 @@ int my_printf(char * restrict format, ...)
     va_start(ap,format);
 
     int format_length = strlen(format);
-
+    int length = 0;
     int i = 0;
     while(i < format_length){
         if(format[i] == '%'){
@@ -16,39 +16,27 @@ int my_printf(char * restrict format, ...)
             {
                 case 's':
                 {
-                    const char *buff = va_arg(ap,const char*);
-                    write(1,buff,strlen(buff));
+                    char *buff = va_arg(ap,char*);
+                    length += write(1,buff,strlen(buff));
                     i++;//increment i twice when format strings are involved so the format character (s,d,c,etc.) are not written to stdput
                     break;
                 }
                 case 'c':
                 {
-                    const char *buff = va_arg(ap,const char*);
-                    write(1,buff,strlen(buff));
+                    int buff = va_arg(ap,int);
+                    printf("%c",(char)buff);
+                    // write(1,buff,1);
+                    
+                    length++;
                     i++;
                     break;
                 }
                 case 'd':
+                case 'u':
                 {   
                     int buff = va_arg(ap,int);
                     char* string = reverse_string(itoa(buff));
-                    write(1,string,strlen(string));
-                    i++;
-                    break;
-                }
-                case 'u':
-                {   /////need to make another itoa function to take in type long
-                    int unsined_input = va_arg(ap,int);
-                    if(unsined_input < 0)
-                    {
-                        long unsind_int = (long)(UINT_MAX + unsined_input + 1);
-                        char* unsind_int_string = reverse_string(ltoa(unsind_int));
-                        write(1,unsind_int_string,strlen(unsind_int_string));
-                        i++;
-                        break;
-                    }                    
-                    char* string = reverse_string(itoa(unsined_input));
-                    write(1,string,strlen(string));
+                    length += write(1,string,strlen(string));
                     i++;
                     break;
                 }
@@ -56,7 +44,7 @@ int my_printf(char * restrict format, ...)
                 {
                     int buff = va_arg(ap,int);
                     char* string = reverse_string(dec_2_oct_to_a(buff));
-                    write(1,string,strlen(string));
+                    length += write(1,string,strlen(string));
                     i++;
                     break;
                 }
@@ -65,8 +53,8 @@ int my_printf(char * restrict format, ...)
                     char* hex = "0x10";
                     int buff = va_arg(ap,int);
                     char* string = reverse_string(dec_2_hex_to_a(buff));
-                    write(1,hex,strlen(hex));
-                    write(1,string,strlen(string));
+                    length += write(1,hex,strlen(hex));
+                    length += write(1,string,strlen(string));
                     i++;
                     break;
                 }
@@ -75,28 +63,29 @@ int my_printf(char * restrict format, ...)
                     void* input = va_arg(ap,void*);
                     int* i = (int*)&input;
 
+                    char* hex = "0x10";
                     if(*i < 0){
                         *i *= -1;
                     }
-                    char* hex = "0x10";
+
                     char* mem_string = reverse_string(dec_2_hex_to_a(*i));
-                    write(1,hex,strlen(hex));
-                    write(1,mem_string,strlen(mem_string));
+                    length += write(1,hex,strlen(hex));
+                    length += write(1,mem_string,strlen(mem_string));
                     i++;
                     break;
                 }
                 
             }
         }
-
-        else if(format[i] != 'p')//may printf p simply because it is the last case statement. check later to see if putting the action below in a default statement works better
-        {
-            char* single_char = &format[i];
-            write(1,single_char,1);//will only print out characters, which are 1 byte in size
+        if(format[i] != '%' && format[i -1] != '%'){
+        
+        char* single_char = &format[i];
+        length += write(1,single_char,1);
         }
         i++;
+
     }
     va_end(ap);
-    return EXIT_SUCCESS;
+    return length;
 }
 
